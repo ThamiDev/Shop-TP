cd05cf1b7a0095568718c2664ee3c1373891f95b
ThamiDev <thamisauvaire30@gmail.com>
2021-10-12 14:45:26 +0200

    add css nav

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './nav.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faTimes, faTshirt, faMale, faFemale, faShoePrints, faUser, faPersonBooth, faHome } from '@fortawesome/free-solid-svg-icons';
 
 const Nav = () => {
 
@@ -12,9 +12,67 @@ const Nav = () => {
 
     return (
         <div className="navigation">
-            <div onClick={closeMenuNav} className="menu-close">
-                <FontAwesomeIcon icon={faTimes} className="icon-close" />
-                <p>CLOSE</p>
+            <div className="top-navigation">
+                <div className="logo-header">
+                    <h2>Mr.</h2>
+                    <h2>B<span>a</span>r<span>a</span></h2>
+                </div>
+                <div onClick={closeMenuNav} className="menu-close">
+                    <FontAwesomeIcon icon={faTimes} />
+                    <p>CLOSE</p>
+                </div>
+            </div>
+            <div className="middel-navigation">
+                <ul>
+                    <li className="item">
+                        <div className="icon-item">
+                            <FontAwesomeIcon icon={faHome} />
+                        </div>
+                        <h3>ACCEUIL</h3>
+                    </li>
+                    <li className="item">
+                        <div className="icon-item">
+                            <FontAwesomeIcon icon={faMale} />
+                        </div>
+                        <h3>HOMME</h3>
+                    </li>
+                    <li className="item">
+                        <div className="icon-item">
+                            <FontAwesomeIcon icon={faFemale} />
+                        </div>
+                        <h3>FEMME</h3>
+                    </li>
+                    <li className="item">
+                        <div className="icon-item">
+                            <FontAwesomeIcon icon={faTshirt} />
+                        </div>
+                        <h3>TSHIRT</h3>
+                    </li>
+                    <li className="item">
+                        <div className="icon-item">
+                            <FontAwesomeIcon icon={faTimes} />
+                        </div>
+                        <h3>PANTALON</h3>
+                    </li>
+                    <li className="item">
+                        <div className="icon-item">
+                            <FontAwesomeIcon icon={faShoePrints} />
+                        </div>
+                        <h3>CHAUSSURES</h3>
+                    </li>
+                </ul>
+            </div>
+            <div className="bottom-navigation">
+                <div className="profil">
+                    <FontAwesomeIcon icon={faUser} />
+                    <p>Profil</p>
+                </div>
+                <div className="deconnexion">
+                    <div className="icon-item">
+                        <FontAwesomeIcon icon={faPersonBooth} />
+                    </div>
+                    <p>Deconnexion</p>
+                </div>
             </div>
         </div>
     );
