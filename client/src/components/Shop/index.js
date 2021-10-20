bc80461bd08cb0bd042b374ed58743218650c86a
ThamiDev <thamisauvaire30@gmail.com>
2021-10-20 10:48:43 +0200

    add componant Shop

--- client/src/components/Shop/index.js
+++ client/src/components/Shop/index.js
@@ -1,5 +1,8 @@
 // == Import des librairies
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 // == Import du style
 import './shop.scss';
@@ -12,8 +15,43 @@ const Shop = () => {
     return (
         <div>
             <Header />
-            <h1>Shop</h1>
-            <Footer />
+            <div className="shop">
+                <table>
+                    <tr>
+                        <th>ARTICLE</th>
+                        <th>QUANTITE</th>
+                        <th>SUPPR.</th>
+                        <th>TOTAL</th>
+                    </tr>
+                    <tr>
+                        <td>Tshirt Bleu</td>
+                        <td>1</td>
+                        <td><FontAwesomeIcon icon={faTimes} className="icon-item" /></td>
+                        <td>29.99€</td>
+                    </tr>
+                    <tr>
+                        <td>Jean Noir</td>
+                        <td>2</td>
+                        <td><FontAwesomeIcon icon={faTimes} className="icon-item" /></td>
+                        <td>54.99€</td>
+                    </tr>
+                    <tr>
+                        <td>Chaussures 43</td>
+                        <td>1</td>
+                        <td><FontAwesomeIcon icon={faTimes} className="icon-item" /></td>
+                        <td>100.99€</td>
+                    </tr>
+                </table>
+                <div className="total">
+                    <h2>TOTAL (Prix avec Taxe) <span>124.99€</span></h2>
+                </div>
+                <div className="valid">
+                    <Link exact to="/" activeClassName="nav-active">
+                        <h2 className="close">FERMER <FontAwesomeIcon icon={faTimes} className="icon-item" /></h2>
+                    </Link>
+                    <h2><span>VALIDER VOTRE PANIER</span></h2>
+                </div>
+            </div>
         </div>
     );
 };
