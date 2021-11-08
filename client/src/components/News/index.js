494543ded991399a5fc2d1c24f7262ed8e7ae305
ThamiDev <thamisauvaire30@gmail.com>
2021-11-08 10:17:48 +0100

    add picture news

--- client/src/components/News/index.js
+++ client/src/components/News/index.js
@@ -4,9 +4,10 @@ import { NavLink, Link } from 'react-router-dom';
 
 // == Import du style
 import './news.scss';
-import img from './robe.jpg';
-import img1 from './robee.jpg';
-import img2 from './veste.jpg';
+import img from './pc4.png';
+import img1 from './pc3.jpg';
+import img2 from './pc2.png';
+import img3 from './pc.jpg';
 
 // == Composant
 const News = () => {
@@ -67,7 +68,7 @@ const News = () => {
                 </NavLink>
                 <NavLink exact to="/article">
                     <div className="product">
-                        <img src={img} alt="description" />
+                        <img src={img3} alt="description" />
                         <div className="description-product">
                             <h3>Robe jaune</h3>
                             <h4>29.99€</h4>
